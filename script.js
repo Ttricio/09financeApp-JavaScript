def5d16eb08c21d48bc2e894013db4f1da176f5b
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-10-18 09:10:02 +0200

    project done

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const cancelBtn = document.querySelector(".cancel")
 const deleteBtn = document.querySelector(".delete")
 const deleteAllBtn = document.querySelector(".delete-all")
 
+const lightStyleBtn = document.querySelector('.light')
+const darkStyleBtn = document.querySelector('.dark')
+
 let root = document.documentElement
 let ID = 0
 let categoryIcon
@@ -111,6 +114,29 @@ const deleteTransatcion = (id) => {
 
 }
 
+const deleteAllTransactions = ()=>{
+    incomeSection.innerHTML = '<h3>Przychód</>'
+    expensesSection.innerHtml = '<h3>Wydatki</h3>'
+    availableMoney.textContent = '0zł'
+    moneyArr=[0]
+}
+
+const changeStyleToLight = () => {
+    root.style.setProperty('--first-color', '#F9F9F9');
+    root.style.setProperty('--second-color', '#14161F');
+    root.style.setProperty('--border-color', 'rgba(0, 0, 0, .2)');
+}
+
+const changeStyleToDark = () => {
+    root.style.setProperty('--first-color', '#14161F');
+    root.style.setProperty('--second-color', '#F9F9F9');
+    root.style.setProperty('--border-color', 'rgba(255, 255, 255, .4)');
+}
+
 addTransactionBtn.addEventListener("click", showPanel)
 cancelBtn.addEventListener("click", closePanel)
 saveBtn.addEventListener("click", checkForm)
+deleteAllBtn.addEventListener('click', deleteAllTransactions)
+lightStyleBtn.addEventListener('click', changeStyleToLight)
+darkStyleBtn.addEventListener('click', changeStyleToDark)
+
